6ad47843e7d82b4906506bff093a2f5d57608747
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-02-05 16:31:57 -0600

    .env

---
+++ .env/zsh.zsh
@@ -0,0 +1,7 @@
+export PROJECT_ROOT=$PWD
+
+load ~/.zsh/environments/helpers.zsh && save-function-list
+load ~/.zsh/environments/basic.zsh
+load ~/.zsh/environments/emacs.zsh
+
+report-custom-functions
